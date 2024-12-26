748f896cbb00fb6f388dc8cbe541be454268407e
Jan Kuhfeld <jan.kuhfeld@rub.de>
2024-12-26 10:31:55 +0900

    add prelimary definitions file format

--- src/ExoMol.jl
+++ src/ExoMol.jl
@@ -2,9 +2,11 @@ module ExoMol
 
 
 include("constants.jl")
+include("definitions.jl")
 include("states.jl")
 include("transitions.jl")
 
+export read_def_file
 export read_state_file, read_trans_file, add_crosssection!, lookup_state_id
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using ExoMol
 using Test
+using SafeTestsets
 
 @testset "ExoMol.jl" begin
     # Write your tests here.
+  @safetestset "Definitions tests" begin include("definitions_tests.jl") end
 end
