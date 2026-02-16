5174548279d0f3607e91be31079e94e5c7bd0476
Jan Kuhfeld <jan@kuhfeld.net>
2026-02-16 11:43:34 +0900

    fix: handling of multiple transition files

--- src/isotopologue.jl
+++ src/isotopologue.jl
@@ -44,14 +44,14 @@ function load_isotopologue(folder)
   states = read_state_file(states_files[1], def)
   if length(states_files) > 1
     for states_file in states_files[2:end]
-      push!(states, read_state_file(states_file, def))
+      append!(states, read_state_file(states_file, def))
     end
   end
 
   transitions = read_trans_file(trans_files[1])
   if length(trans_files) > 1
     for trans_file in trans_files[2:end]
-      push!(transitions, read_trans_file(trans_file))
+      append!(transitions, read_trans_file(trans_file))
     end
   end
 
